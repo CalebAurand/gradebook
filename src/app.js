fc2455f5471cd065a2ba72247246c754bd8ec132
CalebAurand <caurand13@gmail.com>
2022-10-07 20:13:24 -0500

    added back student login url

--- src/app.js
+++ src/app.js
@@ -17,9 +17,9 @@ app.use(cors({
   origin: ['https://home-grade-book.vercel.app']
 }));
 
-// app.use(cors({
-//   origin: ['https://home-grade-book.vercel.app/student-login']
-// }));
+app.use(cors({
+  origin: ['https://home-grade-book.vercel.app/student-login']
+}));
 
 // tell the app to use the bodyParser.json() method to be able to read json
 app.use(bodyParser.json());
